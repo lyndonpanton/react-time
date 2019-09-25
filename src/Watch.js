5ebbf2a47b428635876e33f081c9d297fb372d69
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-25 20:44:39 +0100

    added function to shorten logic

--- src/Watch.js
+++ src/Watch.js
@@ -20,66 +20,20 @@ class Watch extends React.Component {
 		}, 1000);
 	}
 
+	setDigit = (digit) => {
+		if (this.state[digit] < 9) {
+			this.setState({
+				[digit]: this.state[digit] + 1
+			});
+		} else {
+			this.setState({
+				[digit]: 0
+			});
+		}
+	}
+
 	tick = () => {
-		if (this.state.secondOne[0] < 9) {
-			this.setState({
-				secondOne: this.state.secondOne + 1
-			});
-		} else {
-			this.setState({
-				secondOne: 0
-			});
-		}
-
-		if (this.setState.secondTen < 9) {
-			this.setState({
-				secondTen: this.state.secondTen + 1
-			});
-		} else {
-			this.setState({
-				secondTen: 0
-			});
-		}
-
-		if (this.setState.minuteOne < 9) {
-			this.setState({
-				minuteOne: this.state.minuteOne + 1
-			});
-		} else {
-			this.setState({
-				minuteOne: 0
-			});
-		}
-
-		if (this.setState.minuteTen < 9) {
-			this.setState({
-				minuteTen: this.state.minuteTen + 1
-			});
-		} else {
-			this.setState({
-				minuteTen: 0
-			});
-		}
-
-		if (this.setState.hourOne < 9) {
-			this.setState({
-				hourOne: this.state.hourOne + 1
-			});
-		} else {
-			this.setState({
-				hourOne: 0
-			});
-		}
-
-		if (this.setState.hourTen < 9) {
-			this.setState({
-				hourTen: this.state.hourTen + 1
-			});
-		} else {
-			this.setState({
-				hourTen: 0
-			});
-		}
+		setDigit("secondOne");
 	}
 
 	render() {
