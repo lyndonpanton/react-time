852758df2fc787cc00e8d64ec9f8d57c19b5f4ba
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-25 20:27:01 +0100

    added initial time state

--- src/Watch.js
+++ src/Watch.js
@@ -2,6 +2,24 @@ import React from "react";
 import Button from "./Button";
 
 class Watch extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			seconds: [
+				0,
+				0
+			],
+			minutes: [
+				0,
+				0
+			],
+			hours: [
+				0,
+				0
+			]
+		}
+	}
+
 	render() {
 
 	}
