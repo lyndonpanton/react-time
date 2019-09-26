c7b183d65fea36360d25f7554268331d3c1851c3
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-26 19:53:19 +0100

    created clock template

--- src/Digital.js
+++ src/Digital.js
@@ -3,7 +3,26 @@ import React from "react";
 class Digital extends React.Component {
 	render() {
 		return (
-			
+			<div className="digital">
+				<div className="time">
+					<div className="hours">
+						<span className="hour">{}</span>
+						<span className="hour">{}</span>
+					</div>
+					<div className="minutes">
+						<span className="minute">{}</span>
+						<span className="minute">{}</span>
+					</div>
+					<div className="seconds">
+						<span className="second">{}</span>
+						<span className="second">{}</span>
+					</div>
+					<div className="milliseconds">
+						<span className="millisecond">{}</span>
+						<span className="millisecond">{}</span>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
