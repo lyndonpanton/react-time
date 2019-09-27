d22bf119225b43a3ad83ba4ff2b4a352ad5cd6c3
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-27 16:44:01 +0100

    changed component to be stateless

--- src/Record.js
+++ src/Record.js
@@ -1,28 +1,9 @@
 import React from "react";
 
-class Record extends React.Component {
-	render() {
-		return (
-			<div className="record">
-				<div className="hours">
-					<span className="hour">{this.props.hourTen}</span>
-					<span className="hour">{this.props.hourOne}</span>
-				</div>
-				<div className="minutes">
-					<span className="minute">{this.props.minuteTen}</span>
-					<span className="minute">{this.props.minuteOne}</span>
-				</div>
-				<div className="seconds">
-					<span className="second">{this.props.secondTen}</span>
-					<span className="second">{this.props.secondOne}</span>
-				</div>
-				<div className="milliseconds">
-					<span className="millisecond">{this.props.millisecondTen}</span>
-					<span className="millisecond">{this.props.millisecondOne}</span>
-				</div>
-			</div>
-		);
-	}
+const Record = () => {
+	return (
+
+	);
 }
 
 export default Record;
