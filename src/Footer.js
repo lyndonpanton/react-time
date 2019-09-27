095708e95e0de978aba4d42116b491a09b0b605b
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-27 19:00:05 +0100

    created component

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = (props) => {
+	// add necessary credits
+	return (
+		<footer>
+			<p className="copyright">
+				<span className="symbol">{props.symbol} </span> 
+				<span className="year"> {props.year} </span> 
+				<span className="author"> {props.author} | </span>
+				<span className="rights"> {props.right}</span>
+			</p>
+			<p className="credits">
+
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
