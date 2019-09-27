40905c0c69a2bcc851d682b950fa7f6cc64106f1
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-27 21:29:24 +0100

    reformatted class names

--- src/Record.js
+++ src/Record.js
@@ -5,8 +5,8 @@ const Record = (props) => {
 	const records = props.records.map(function(record, index) {
 		return (
 			<div key={index} className="record">
-				<span className="recordIndex">{index + 1}</span>
-				<div className="recordTime">
+				<span className="record-index">{index + 1}</span>
+				<div className="record-time">
 					<span className="hour">{record.hourTen}{record.hourOne}:</span>
 					<span className="minute">{record.minuteTen}{record.minuteOne}:</span>
 					<span className="second">{record.secondTen}{record.secondOne}:</span>
