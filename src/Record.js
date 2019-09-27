278f6b9863bd59844199ac63e6d16ddf69c32e8e
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-27 17:54:40 +0100

    added data to component

--- src/Record.js
+++ src/Record.js
@@ -1,6 +1,20 @@
 import React from "react";
 
-const Record = () => {
+const Record = (props) => {
+	const records = props.records.map(function(record, index) {
+		return (
+			<div key={index} className="record">
+				<span className="recordIndex">{index + 1}</span>
+				<span className="recordTime">
+					{record.hourTen}{record.hourOne} : 
+					{record.minuteTen}{record.minuteOne} :
+					{record.secondTen}{record.secondOne} :
+					{record.millisecondTen}{record.millisecondOne}
+				</span>
+			</div>
+		);
+	});
+	
 	return (
 
 	);
