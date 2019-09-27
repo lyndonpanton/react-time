4c798f6991ef51c6cd9e2f52498a19d459dad686
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-27 18:54:13 +0100

    added current year to state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import Watch from "./Watch";
 import "./index.css";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			year: (new Date()).getFullYear()
+		};
+	}
+	
 	render() {
 		return (
 			<div className="app">
