9aaabf5984294e0fc3c55086f0103f2e7ebe6d73
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-28 18:34:35 +0100

    created file template

---
+++ src/Analog.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Analog extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div className="analog">
+				TODO
+			</div>
+		);
+	}
+}
+
+export default Analog;
