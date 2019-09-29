565f6c992d6c795655fd07ba7bd623008913c9da
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-29 18:06:46 +0100

    imported button component, added button to component

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./Button";
 
 class Pomodoro extends React.Component {
 	constructor(props) {
@@ -14,55 +15,59 @@ class Pomodoro extends React.Component {
 	}
 
 	add = (digit) => {
-		
+
 	}
 
 	minus = (digit) => {
-		
+
 	}
 
 	tick = () => {
-		
+
 	}
 
 	render() {
 		return (
 			<div className="pomodoro">
-				<div className="column">
-					<div className="arrow-up" tabIndex="0" onClick={() => this.add("hourTen")}></div>
-					<div className="digit">{this.state.hourTen}</div>
-					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("hourTen")}></div>
+				<div className="pomodoro-clock">
+					<div className="column">
+						<div className="arrow-up" tabIndex="0" onClick={() => this.add("hourTen")}></div>
+						<div className="digit">{this.state.hourTen}</div>
+						<div className="arrow-down" tabIndex="0" onClick={() => this.minus("hourTen")}></div>
+					</div>
+
+					<div className="column">
+						<div className="arrow-up" tabIndex="0" onClick={() => this.add("hourOne")}></div>
+						<div className="digit">{this.state.hourOne}</div>
+						<div className="arrow-down" tabIndex="0" onClick={() => this.minus("hourOne")}></div>
+					</div>
+
+					<div className="column">
+						<div className="arrow-up" tabIndex="0" onClick={() => this.add("minuteTen")}></div>
+						<div className="digit">{this.state.minuteTen}</div>
+						<div className="arrow-down" tabIndex="0" onClick={() => this.minus("minuteTen")}></div>
+					</div>
+
+					<div className="column">
+						<div className="arrow-up" tabIndex="0" onClick={() => this.add("minuteOne")}></div>
+						<div className="digit">{this.state.minuteOne}</div>
+						<div className="arrow-down" tabIndex="0" onClick={() => this.minus("minuteOne")}></div>
+					</div>
+
+					<div className="column">
+						<div className="arrow-up" tabIndex="0" onClick={() => this.add("secondTen")}></div>
+						<div className="digit">{this.state.secondTen}</div>
+						<div className="arrow-down" tabIndex="0" onClick={() => this.minus("secondTen")}></div>
+					</div>
+
+					<div className="column">
+						<div className="arrow-up" tabIndex="0" onClick={() => this.add("secondOne")}></div>
+						<div className="digit">{this.state.secondOne}</div>
+						<div className="arrow-down" tabIndex="0" onClick={() => this.minus("secondOne")}></div>
+					</div>
 				</div>
 
-				<div className="column">
-					<div className="arrow-up" tabIndex="0" onClick={() => this.add("hourOne")}></div>
-					<div className="digit">{this.state.hourOne}</div>
-					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("hourOne")}></div>
-				</div>
-
-				<div className="column">
-					<div className="arrow-up" tabIndex="0" onClick={() => this.add("minuteTen")}></div>
-					<div className="digit">{this.state.minuteTen}</div>
-					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("minuteTen")}></div>
-				</div>
-
-				<div className="column">
-					<div className="arrow-up" tabIndex="0" onClick={() => this.add("minuteOne")}></div>
-					<div className="digit">{this.state.minuteOne}</div>
-					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("minuteOne")}></div>
-				</div>
-
-				<div className="column">
-					<div className="arrow-up" tabIndex="0" onClick={() => this.add("secondTen")}></div>
-					<div className="digit">{this.state.secondTen}</div>
-					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("secondTen")}></div>
-				</div>
-
-				<div className="column">
-					<div className="arrow-up" tabIndex="0" onClick={() => this.add("secondOne")}></div>
-					<div className="digit">{this.state.secondOne}</div>
-					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("secondOne")}></div>
-				</div>
+				<Button type="countdown" handleClick={() => this.tick()} />
 			</div>
 		);
 	} 
