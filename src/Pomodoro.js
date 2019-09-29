5a95670ffd33f540d4c2c45cd7020692b9099563
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-29 17:58:08 +0100

    corrected attribute spelling

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -14,11 +14,11 @@ class Pomodoro extends React.Component {
 	}
 
 	add = (digit) => {
-
+		
 	}
 
 	minus = (digit) => {
-
+		
 	}
 
 	tick = () => {
@@ -29,39 +29,39 @@ class Pomodoro extends React.Component {
 		return (
 			<div className="pomodoro">
 				<div className="column">
-					<div className="arrow-up" tabindex="0" onClick={() => this.add("hourTen")}></div>
+					<div className="arrow-up" tabIndex="0" onClick={() => this.add("hourTen")}></div>
 					<div className="digit">{this.state.hourTen}</div>
-					<div className="arrow-down" tabindex="0" onClick={() => this.minus("hourTen")}></div>
+					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("hourTen")}></div>
 				</div>
 
 				<div className="column">
-					<div className="arrow-up" tabindex="0" onClick={() => this.add("hourOne")}></div>
+					<div className="arrow-up" tabIndex="0" onClick={() => this.add("hourOne")}></div>
 					<div className="digit">{this.state.hourOne}</div>
-					<div className="arrow-down" tabindex="0" onClick={() => this.minus("hourOne")}></div>
+					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("hourOne")}></div>
 				</div>
 
 				<div className="column">
-					<div className="arrow-up" tabindex="0" onClick={() => this.add("minuteTen")}></div>
+					<div className="arrow-up" tabIndex="0" onClick={() => this.add("minuteTen")}></div>
 					<div className="digit">{this.state.minuteTen}</div>
-					<div className="arrow-down" tabindex="0" onClick={() => this.minus("minuteTen")}></div>
+					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("minuteTen")}></div>
 				</div>
 
 				<div className="column">
-					<div className="arrow-up" tabindex="0" onClick={() => this.add("minuteOne")}></div>
+					<div className="arrow-up" tabIndex="0" onClick={() => this.add("minuteOne")}></div>
 					<div className="digit">{this.state.minuteOne}</div>
-					<div className="arrow-down" tabindex="0" onClick={() => this.minus("minuteOne")}></div>
+					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("minuteOne")}></div>
 				</div>
 
 				<div className="column">
-					<div className="arrow-up" tabindex="0" onClick={() => this.add("secondTen")}></div>
+					<div className="arrow-up" tabIndex="0" onClick={() => this.add("secondTen")}></div>
 					<div className="digit">{this.state.secondTen}</div>
-					<div className="arrow-down" tabindex="0" onClick={() => this.minus("secondTen")}></div>
+					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("secondTen")}></div>
 				</div>
 
 				<div className="column">
-					<div className="arrow-up" tabindex="0" onClick={() => this.add("secondOne")}></div>
+					<div className="arrow-up" tabIndex="0" onClick={() => this.add("secondOne")}></div>
 					<div className="digit">{this.state.secondOne}</div>
-					<div className="arrow-down" tabindex="0" onClick={() => this.minus("secondOne")}></div>
+					<div className="arrow-down" tabIndex="0" onClick={() => this.minus("secondOne")}></div>
 				</div>
 			</div>
 		);
