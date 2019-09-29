09043da9ebd4e7e4b1b3f30bed49febd20134b74
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-29 14:21:09 +0100

    added digits to state

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -4,7 +4,12 @@ class Pomodoro extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			secondOne: 0,
+			secondTen: 0,
+			minuteOne: 0,
+			minuteTen: 0,
+			hourOne: 0,
+			hourTen: 0
 		};
 	}
 
@@ -13,37 +18,37 @@ class Pomodoro extends React.Component {
 			<div className="pomodoro">
 				<div className="column">
 					<div className="arrow-up"></div>
-					<div className="digit">0</div>
+					<div className="digit">{this.state.hourTen}</div>
 					<div className="arrow-down"></div>
 				</div>
 
 				<div className="column">
 					<div className="arrow-up"></div>
-					<div className="digit">0</div>
+					<div className="digit">{this.state.hourOne}</div>
 					<div className="arrow-down"></div>
 				</div>
 
 				<div className="column">
 					<div className="arrow-up"></div>
-					<div className="digit">0</div>
+					<div className="digit">{this.state.minuteTen}</div>
 					<div className="arrow-down"></div>
 				</div>
 
 				<div className="column">
 					<div className="arrow-up"></div>
-					<div className="digit">0</div>
+					<div className="digit">{this.state.minuteOne}</div>
 					<div className="arrow-down"></div>
 				</div>
 
 				<div className="column">
 					<div className="arrow-up"></div>
-					<div className="digit">0</div>
+					<div className="digit">{this.state.secondTen}</div>
 					<div className="arrow-down"></div>
 				</div>
 
 				<div className="column">
 					<div className="arrow-up"></div>
-					<div className="digit">0</div>
+					<div className="digit">{this.state.secondOne}</div>
 					<div className="arrow-down"></div>
 				</div>
 			</div>
